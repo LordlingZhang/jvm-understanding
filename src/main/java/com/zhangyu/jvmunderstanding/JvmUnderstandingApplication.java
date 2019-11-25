3b66010e1f40392773833263a814a49f1ad508ca
LordlingZhang <852611546@qq.com>
2019-11-26 00:52:22 +0800

    jvm学习

---
+++ src/main/java/com/zhangyu/jvmunderstanding/JvmUnderstandingApplication.java
@@ -0,0 +1,13 @@
+package com.zhangyu.jvmunderstanding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JvmUnderstandingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JvmUnderstandingApplication.class, args);
+	}
+
+}
