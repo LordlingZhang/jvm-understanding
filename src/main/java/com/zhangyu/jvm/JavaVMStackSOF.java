99b12ea8044ee57806c17c301fb1698d538dd972
LordlingZhang <852611546@qq.com>
2019-11-26 23:41:45 +0800

    StackOverflow生成

---
+++ src/main/java/com/zhangyu/jvm/JavaVMStackSOF.java
@@ -0,0 +1,32 @@
+package com.zhangyu.jvm;
+
+/**
+ * @author : zhangyu
+ * @description : java虚拟机栈 栈溢出 异常
+ * VM args : -Xss160k
+ * @date : 2019/11/26
+ */
+public class JavaVMStackSOF {
+
+    private int stackLength = 1;
+
+    public static void main(String[] args){
+        JavaVMStackSOF vm = new JavaVMStackSOF();
+        try {
+            vm.stackLeak();
+        } catch (Throwable e) {
+            System.out.println("当前栈深度："+ vm.stackLength);
+            throw e;
+        }
+
+    }
+
+    public  void stackLeak(){
+        stackLength ++;
+        stackLeak();
+    }
+
+
+
+
+}
